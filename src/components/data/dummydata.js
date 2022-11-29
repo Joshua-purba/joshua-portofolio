dd941edf0d7f3ee18d3f67c5607f343393ecd0d8
Joshua-Purba <joshuar260602@gmail.com>
2022-11-30 00:35:01 +0800

    menambahkan file joshua-portofolio

---
+++ src/components/data/dummydata.js
@@ -0,0 +1,232 @@
+import { Settings, CropRotate, ViewInAr, PieChart, Code, BarChart, CloudOutlined, FavoriteBorder, Public, PersonOutlined, AddLocationAltOutlined, PhoneIphone, EmailOutlined, Facebook, Twitter, Instagram, YouTube } from "@mui/icons-material"
+
+export const navlink = [
+  {
+    url: "/",
+    text: "Home",
+  },
+  {
+    url: "/about",
+    text: "About",
+  },
+  {
+    url: "/services",
+    text: "Services",
+  },
+  {
+    url: "/portfolio",
+    text: "Portfolio",
+  },
+  {
+    url: "/testimonials",
+    text: "The Experts",
+  },
+  {
+    url: "/blog",
+    text: "Blog",
+  },
+  {
+    url: "/contact",
+    text: "Contact",
+  },
+]
+export const home = [
+  {
+    text: "HELLO I'M",
+    name: "JOSHUA PURBA",
+    post: "FRONT END STUDENT",
+    design: "UI / UX DESIGNER",
+    desc: "“The trend of design toward simplicity and accessibility in software happened for a reason—simple sells, simple’s usable and simple scales.” — Rand Fishkin, Founder at Moz",
+  },
+]
+export const about = [
+  {
+    desc: "I am Joshua Purba, an undergraduate student in the information systems study program at the Klabat University campus with a specialty in the field of UI/UX design. I am adaptable, can work hard, work together in a team or group, am meticulous, easy to communicate with others, and like to learn new things.",
+    cover: "./images/WhatsApp Image 2021-01-30 at 6.59.32 PM (1) (1).png",
+  },
+]
+export const services = [
+  {
+    id: 1,
+    icon: <Settings />,
+    title: "Design",
+    desc: "Design is about progress. It is the conceptualization and creation of new things: ideas, interactions, information, objects, typefaces, books, posters, signs, systems services, websites, and more",
+  },
+  {
+    id: 2,
+    icon: <CropRotate />,
+    title: "Designer",
+    desc: "Designers imagine and make. They also research and think. Skilled in one or more specialties of the discipline, designers use their abilities in collaboration with others.",
+  },
+  {
+    id: 3,
+    icon: <ViewInAr />,
+    title: "Responsive Design",
+    desc: "Responsive design is an approach that suggests that design and development should respond to user behavior and environment based on screen size, platform, and orientation.",
+  },
+  {
+    id: 4,
+    icon: <PieChart />,
+    title: "Material UI	",
+    desc: "Material UI is an open-source React component library that implements Google's material dessign. It includes a comprehensive collection of prebuilt components that are ready for use in production.",
+  },
+  {
+    id: 5,
+    icon: <Code />,
+    title: "Material UI Icons",
+    desc: "A UI icon is a symbol used for computer interfaces and is usually a button or link. The home symbol, app buttons on mobile devices, and the minimize window button are all examples of UI icons.",
+  },
+  {
+    id: 6,
+    icon: <BarChart />,
+    title: "Awesome Support",
+    desc: "We are very happy and proud as UI/UX designers if you (clients) are very happy with the product or results that we are working on.",
+  },
+]
+export const project = [
+  {
+    id: 1,
+    icon: <CloudOutlined />,
+    num: "89",
+    title: "SKS COMPLEATED",
+  },
+  {
+    id: 2,
+    icon: <FavoriteBorder />,
+    num: "150",
+    title: "COURSEWORK COMPLEATED",
+  },
+  {
+    id: 3,
+    icon: <Public />,
+    num: "4",
+    title: "SEMESTER COMPLEATED",
+  },
+  {
+    id: 4,
+    icon: <PersonOutlined />,
+    num: "1446",
+    title: "LIENS OF CODE",
+  },
+]
+export const portfolio = [
+  {
+    id: 1,
+    cover: "../images/port/gambar1.jpg",
+    name: "Brand",
+    category: "marketing",
+    title: "Brex Logo",
+  },
+  {
+    id: 2,
+    cover: "../images/port/gambar2.jpg",
+    name: "Brand",
+    category: "design",
+    title: "Brex Logo",
+  },
+  {
+    id: 3,
+    cover: "../images/port/gambar3.jpg",
+    name: "Brand",
+    category: "development",
+    title: "Brex Logo",
+  },
+  {
+    id: 4,
+    cover: "../images/port/gambar4.jpg",
+    name: "Brand",
+    category: "marketing",
+    title: "Brex Logo",
+  },
+  {
+    id: 5,
+    cover: "../images/port/gambar5.jpg",
+    name: "Brand",
+    category: "design",
+    title: "Brex Logo",
+  },
+  {
+    id: 6,
+    cover: "../images/port/gambar6.jpg",
+    name: "Brand",
+    category: "development",
+    title: "Brex Logo",
+  },
+]
+export const testimonials = [
+  {
+    id: 1,
+    text: "Taufiq Syahrir, who serves as Head of UI/UX at the popular payment platform LinkAja, is a graduate of S1 Informatics and S2 Digital Media & Games and used to make apps and games a lot.",
+    image: "./images/testimonials/taufik.png",
+    name: "Taufiq Syahrir",
+    post: "UI/UX Designer",
+  },
+  {
+    id: 2,
+    text: "Hatta Zakaria is a UX researcher at LinkAja and is currently also contributing to the development of the UXiD Jakarta community.",
+    image: "./images/testimonials/Hattazakaria.png",
+    name: "Hatta Zakaria",
+    post: "UX Researcher",
+  },
+  {
+    id: 3,
+    text: "Moga Adiangga works as a product designer at Bukalapak, one of Indonesia's largest e-commerce companies. Moga's experience in the design industry dates back to college, when he frequently competed in mobile application design competitions, worked as a freelance UI/UX designer, and even chose UI/UX as his final thesis topic.",
+    image: "./images/testimonials/mogaAdiangga.png",
+    name: "Moga Adiangga",
+    post: "Product Manager",
+  },
+]
+export const blog = [
+  {
+    id: 1,
+    title: "Master These Awesome",
+    date: "Jun 27, 2022",
+    author: "Dorian Gray",
+    cover: "./images/blog/b1.png",
+  },
+  {
+    id: 2,
+    title: "Best Design Items to Appeal",
+    date: "Jun 27, 2022",
+    author: "Dorian Gray",
+    cover: "./images/blog/b2.png",
+  },
+  {
+    id: 3,
+    title: "The 20 Best Lightroom Presets",
+    date: "Jun 27, 2022",
+    author: "Dorian Gray",
+    cover: "./images/blog/b3.png",
+  },
+]
+export const contact = [
+  {
+    icon: <AddLocationAltOutlined />,
+    text1: "Jl.Getsemani, Manibang, Malalayang satu Barat",
+    text2: "Manado, Sulawesi Utara, 95163",
+  },
+  {
+    icon: <PhoneIphone />,
+    text1: "+6282188172334",
+    
+  },
+  {
+    icon: <EmailOutlined />,
+    text1: "s2200644@student.unklab.ac.id",
+    text2: "joshuar260602@gmail.com",
+  },
+]
+export const social = [
+  {
+    icon: <Facebook />,
+  },
+  {
+    icon: <Twitter />,
+  },
+  {
+    icon: <Instagram />,
+  },
+  {
+    icon: <YouTube />,
+  },
+]
